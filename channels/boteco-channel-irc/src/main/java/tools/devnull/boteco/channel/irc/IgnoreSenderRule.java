5092f31ca50f551b6a1ac86d2c3a9f20b74d99fd
Ataxexe <ataxexe@devnull.tools>
2016-05-25 23:53:07 -0300

    implement the ignore list as a rule

---
+++ channels/boteco-channel-irc/src/main/java/tools/devnull/boteco/channel/irc/IgnoreSenderRule.java
@@ -0,0 +1,49 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2016 Marcelo "Ataxexe" Guimarães <ataxexe@devnull.tools>
+ *
+ * Permission  is hereby granted, free of charge, to any person obtaining
+ * a  copy  of  this  software  and  associated  documentation files (the
+ * "Software"),  to  deal  in the Software without restriction, including
+ * without  limitation  the  rights to use, copy, modify, merge, publish,
+ * distribute,  sublicense,  and/or  sell  copies of the Software, and to
+ * permit  persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * The  above  copyright  notice  and  this  permission  notice  shall be
+ * included  in  all  copies  or  substantial  portions  of the Software.
+ *
+ * THE  SOFTWARE  IS  PROVIDED  "AS  IS",  WITHOUT  WARRANTY OF ANY KIND,
+ * EXPRESS  OR  IMPLIED,  INCLUDING  BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN  NO  EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM,  DAMAGES  OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT  OR  OTHERWISE,  ARISING  FROM,  OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE   OR   THE   USE   OR   OTHER   DEALINGS  IN  THE  SOFTWARE.
+ */
+
+package tools.devnull.boteco.channel.irc;
+
+import tools.devnull.boteco.Rule;
+import tools.devnull.boteco.message.IncomeMessage;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A rule to ignore IRC Messages from some senders (useful if you need to ignore other bots).
+ */
+public class IgnoreSenderRule implements Rule {
+
+  private final List<String> ignored;
+
+  public IgnoreSenderRule(String values) {
+    this.ignored = Arrays.asList(values.split("\\s*,\\s*"));
+  }
+
+  @Override
+  public boolean accept(IncomeMessage message) {
+    return !ignored.contains(message.sender().username());
+  }
+
+}
