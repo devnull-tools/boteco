1902674c3384b3b6d7e45f9bfbb270d197351d32
Ataxexe <ataxexe@devnull.tools>
2016-08-22 15:05:20 -0300

    move to the correspondent package

--- main/boteco-base/src/test/java/tools/devnull/boteco/message/MessageCheckerTest.java
+++ main/boteco-base/src/test/java/tools/devnull/boteco/message/MessageCheckerTest.java
@@ -22,7 +22,7 @@
  * SOFTWARE   OR   THE   USE   OR   OTHER   DEALINGS  IN  THE  SOFTWARE.
  */
 
-package tools.devnull.boteco;
+package tools.devnull.boteco.message;
 
 import org.junit.Before;
 import org.junit.Test;
