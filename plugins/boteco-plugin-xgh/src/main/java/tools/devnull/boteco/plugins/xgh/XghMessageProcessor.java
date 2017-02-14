957a236ca66538fc35b5be95069c7d4e08ebebba
Ataxexe <ataxexe@devnull.tools>
2017-02-14 00:43:50 -0200

    create the eXtreme Go Horse plugin

---
+++ plugins/boteco-plugin-xgh/src/main/java/tools/devnull/boteco/plugins/xgh/XghMessageProcessor.java
@@ -0,0 +1,47 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2017 Marcelo "Ataxexe" Guimarães <ataxexe@devnull.tools>
+ *
+ * Permission  is hereby granted, free of charge, to any person obtaining
+ * a  copy  of  this  software  and  associated  documentation files (the
+ * "Software"),  to  deal  in the Software without restriction, including
+ * without  limitation  the  rights to use, copy, modify, merge, publish,
+ * distribute,  sublicense,  and/or  sell  copies of the Software, and to
+ * permit  persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * The  above  copyright  notice  and  this  permission  notice  shall be
+ * included  in  all  copies  or  substantial  portions  of the Software.
+ *
+ * THE  SOFTWARE  IS  PROVIDED  "AS  IS",  WITHOUT  WARRANTY OF ANY KIND,
+ * EXPRESS  OR  IMPLIED,  INCLUDING  BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN  NO  EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM,  DAMAGES  OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT  OR  OTHERWISE,  ARISING  FROM,  OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE   OR   THE   USE   OR   OTHER   DEALINGS  IN  THE  SOFTWARE.
+ */
+
+package tools.devnull.boteco.plugins.xgh;
+
+import tools.devnull.boteco.message.IncomeMessage;
+import tools.devnull.boteco.message.MessageProcessor;
+import tools.devnull.boteco.message.checker.Command;
+
+@Command("xgh")
+public class XghMessageProcessor implements MessageProcessor {
+
+  private final XGH xgh = new XGH();
+
+  @Override
+  public void process(IncomeMessage message) {
+    Integer axiomNumber = message.command().as(Integer.class);
+    Axiom axiom = xgh.axiom(axiomNumber);
+    if (axiom != null) {
+      message.reply("[a]Axiom #%d:[/a][aa]%s[/aa]%n%s", axiom.getNumber(), axiom.getTitle(), axiom.getDescription());
+    } else {
+      message.reply("Axiom %d not defined", axiomNumber);
+    }
+  }
+
+}
