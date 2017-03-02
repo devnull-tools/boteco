5649a6d96709dbc67a0be3bb7044c1a2c139a092
Ataxexe <ataxexe@devnull.tools>
2017-03-02 12:44:07 -0300

    rename module

---
+++ plugins/boteco-plugin-manager/src/main/java/tools/devnull/boteco/plugins/activation/PluginManagerMessageProcessor.java
@@ -0,0 +1,69 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2017 Marcelo "Ataxexe" Guimarães <ataxexe@devnull.tools>
+ *
+ * Permission  is hereby granted, free of charge, to any person obtaining
+ * a  copy  of  this  software  and  associated  documentation files (the
+ * "Software"),  to  deal  in the Software without restriction, including
+ * without  limitation  the  rights to use, copy, modify, merge, publish,
+ * distribute,  sublicense,  and/or  sell  copies of the Software, and to
+ * permit  persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * The  above  copyright  notice  and  this  permission  notice  shall be
+ * included  in  all  copies  or  substantial  portions  of the Software.
+ *
+ * THE  SOFTWARE  IS  PROVIDED  "AS  IS",  WITHOUT  WARRANTY OF ANY KIND,
+ * EXPRESS  OR  IMPLIED,  INCLUDING  BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN  NO  EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM,  DAMAGES  OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT  OR  OTHERWISE,  ARISING  FROM,  OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE   OR   THE   USE   OR   OTHER   DEALINGS  IN  THE  SOFTWARE.
+ */
+
+package tools.devnull.boteco.plugins.activation;
+
+import tools.devnull.boteco.AlwaysActive;
+import tools.devnull.boteco.message.IncomeMessage;
+import tools.devnull.boteco.message.MessageProcessor;
+import tools.devnull.boteco.message.checker.Command;
+import tools.devnull.boteco.plugins.activation.spi.PluginManager;
+
+import java.util.List;
+
+/**
+ * A message processor that can manage plugins for a specific channel and target.
+ */
+@AlwaysActive
+@Command("plugin")
+public class PluginManagerMessageProcessor implements MessageProcessor {
+
+  private final PluginManager activator;
+
+  public PluginManagerMessageProcessor(PluginManager activator) {
+    this.activator = activator;
+  }
+
+  @Override
+  public void process(IncomeMessage message) {
+    message.command()
+        .on("enabled", String.class, (name) -> {
+          boolean active = activator.isEnabled(name, message.location());
+          if (active) {
+            message.reply("[p]Yes, the plugin is enabled for this channel[/p]");
+          } else {
+            message.reply("[n]No, the plugin is disabled for this channel[/n]");
+          }
+        })
+        .on("disable", List.class, (plugins) -> {
+          plugins.forEach(name -> activator.disable(name.toString(), message.location()));
+          message.reply("Plugin(s) disabled successfully!");
+        })
+        .on("enable", List.class, (plugins) -> {
+          plugins.forEach(name -> activator.enable(name.toString(), message.location()));
+          message.reply("Plugin(s) enabled successfully!");
+        });
+  }
+
+}
---
+++ plugins/boteco-plugin-manager/src/main/java/tools/devnull/boteco/plugins/activation/spi/PluginManager.java
@@ -0,0 +1,62 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2017 Marcelo "Ataxexe" Guimarães <ataxexe@devnull.tools>
+ *
+ * Permission  is hereby granted, free of charge, to any person obtaining
+ * a  copy  of  this  software  and  associated  documentation files (the
+ * "Software"),  to  deal  in the Software without restriction, including
+ * without  limitation  the  rights to use, copy, modify, merge, publish,
+ * distribute,  sublicense,  and/or  sell  copies of the Software, and to
+ * permit  persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * The  above  copyright  notice  and  this  permission  notice  shall be
+ * included  in  all  copies  or  substantial  portions  of the Software.
+ *
+ * THE  SOFTWARE  IS  PROVIDED  "AS  IS",  WITHOUT  WARRANTY OF ANY KIND,
+ * EXPRESS  OR  IMPLIED,  INCLUDING  BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN  NO  EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM,  DAMAGES  OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT  OR  OTHERWISE,  ARISING  FROM,  OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE   OR   THE   USE   OR   OTHER   DEALINGS  IN  THE  SOFTWARE.
+ */
+
+package tools.devnull.boteco.plugins.activation.spi;
+
+import tools.devnull.boteco.MessageLocation;
+
+/**
+ * Interface that defines a plugin manager that acts for specific channels.
+ * <p>
+ * Note that for global management, the best is to use the runtime platform itself.
+ */
+public interface PluginManager {
+
+  /**
+   * Disables the given plugin name to run for the given message location
+   *
+   * @param name     the name of the message processor
+   * @param location the message location to refer to
+   */
+  void disable(String name, MessageLocation location);
+
+  /**
+   * Returns {@code true} if the given message processor's name is active
+   * for the given location.
+   *
+   * @param name     the name of the message processor
+   * @param location the message location to refer to
+   * @return {@code true} if the referring parameters represents an active message processor
+   */
+  boolean isEnabled(String name, MessageLocation location);
+
+  /**
+   * Enables the given plugin name to run for the given message location
+   *
+   * @param name     the name of the message processor
+   * @param location the message location to refer to
+   */
+  void enable(String name, MessageLocation location);
+
+}
